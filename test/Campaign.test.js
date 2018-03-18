a5ad9354a4266ed57eb4d793ecb0bfa9edd3b3bb
liebeskind <daniel.liebeskind@gmail.com>
2018-03-18 20:51:19 +0800

    Sets up test

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -5,3 +5,16 @@ const web3 = new Web3(ganache.provider());
 
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
 const compiledCampaign = require('../ethereum/build/Campaign.json');
+
+let accounts;
+let factory;
+let campaignAddress;
+let campaign;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts();
+
+  factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface)) // Passes in the ABI/Interface. Contract takes object, not JSON.
+  .deploy({ data: compiledFactory.bytecode }) // Deploy new instance of the factory.
+  .send({ from: accounts[0], gas: '1000000' })
+})
