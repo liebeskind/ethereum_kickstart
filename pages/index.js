0afad14f71daa22666754e966ff761411c098a04
liebeskind <daniel.liebeskind@gmail.com>
2018-03-21 17:34:50 +0800

    Adds a layout file with header and footer

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 import { Card, Button } from 'semantic-ui-react';
+import Layout from '../components/Layout';
 
 class CampaignIndex extends Component {
   
@@ -19,19 +20,24 @@ class CampaignIndex extends Component {
       };
     });
 
-    return <Card.Group items={items} />
+    return <Card.Group items={items} />;
   }
 
   render() {
-    return <div>
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
-    {this.renderCampaigns()}
-    <Button  
-      content="Create Campaign"
-      icon="add circle"
-      primary // Same thing as primary={true}.
-    />
-    </div>
+    return (
+      <Layout>
+        <div>
+          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+          <h3>Open Campaigns</h3>
+          {this.renderCampaigns()}
+          <Button  
+            content="Create Campaign"
+            icon="add circle"
+            primary // Same thing as primary={true}.
+          />
+        </div>;
+      </Layout>
+    )
   }
 }
 
