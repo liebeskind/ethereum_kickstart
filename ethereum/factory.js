85b125e443910060066911b45b6125a4ed6db6e1
liebeskind <daniel.liebeskind@gmail.com>
2018-03-21 16:09:46 +0800

    Handles issue where don't have access to window (because next.js rendering on server) or where user doesn't have Metamask.

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,5 +1,5 @@
 import web3 from './web3'; // Using instance from version we created.
-import campaignFactory from './build/campaignFactory.json';
+import campaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(campaignFactory.interface), // ABI
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,14 @@
 import Web3 from 'web3';
+const infuraToken = require('../infuraToken');
 
-const web3 = new Web3(window.web3.currentProvider);
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') { // See if we are in the browser and metamask has injected web3 (is running).
+  web3 = new Web3(window.web3.currentProvider); // Using this for version control rather than relying on the version injected by Metamask (we won't know the version).
+} else { // Not in browser because being rendered on server side by next.js
+  // We are not on the server *OR* the user is not running metamask.
+  const provider = new Web3.providers.HttpProvider(infuraToken);
+  web3 = new Web3(provider);
+}
 
 export default web3;
