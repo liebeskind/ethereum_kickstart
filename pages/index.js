04d9231cd9c3f69e4576dbb434afa4732ea84522
liebeskind <daniel.liebeskind@gmail.com>
2018-03-21 17:14:14 +0800

    Adds button

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import factory from '../ethereum/factory';
-import { Card } from 'semantic-ui-react';
+import { Card, Button } from 'semantic-ui-react';
 
 class CampaignIndex extends Component {
   
@@ -26,6 +26,11 @@ class CampaignIndex extends Component {
     return <div>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
     {this.renderCampaigns()}
+    <Button  
+      content="Create Campaign"
+      icon="add circle"
+      primary // Same thing as primary={true}.
+    />
     </div>
   }
 }
