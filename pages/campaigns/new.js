39b036e59af3da697022c444d0830e1602a61f77
liebeskind <daniel.liebeskind@gmail.com>
2018-03-21 20:10:44 +0800

    Adds ui-semantic-react form

---
+++ pages/campaigns/new.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Form, Button, Input } from 'semantic-ui-react';
+import Layout from '../../components/Layout';
+
+
+class CampaignNew extends Component {
+  state = {
+    minimumContribution: ''
+  }
+
+  render() {
+    return (
+      <Layout>
+        <h3>Create a Campaign</h3>
+        
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input 
+              label="wei" 
+              labelPosition="right"
+              value={this.state.minimumContribution}
+              onChange={event => 
+                this.setState({ minimumContribution: event.target.value })}
+            />
+          </Form.Field>
+
+          <Button primary>Create!</Button>
+        </Form>
+
+      </Layout>
+    )
+  }
+};
+
+export default CampaignNew;
--- pages/index.js
+++ pages/index.js
@@ -27,14 +27,14 @@ class CampaignIndex extends Component {
     return (
       <Layout>
         <div>
-          <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
           <h3>Open Campaigns</h3>
-          {this.renderCampaigns()}
           <Button  
             content="Create Campaign"
             icon="add circle"
+            floated="right"
             primary // Same thing as primary={true}.
           />
+          {this.renderCampaigns()}
         </div>;
       </Layout>
     )
