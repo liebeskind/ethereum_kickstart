6b22a6e3e9af69d7d77d9b7bd61a51a73f4b2be7
liebeskind <daniel.liebeskind@gmail.com>
2018-03-21 17:08:56 +0800

    Adds semantic ui styling

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,10 @@ class CampaignIndex extends Component {
   }
 
   render() {
-    return <div>{this.renderCampaigns()}</div>
+    return <div>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"></link>
+    {this.renderCampaigns()}
+    </div>
   }
 }
 
