0c22e500994c69430540982f59f05419ade6f540
liebeskind <daniel.liebeskind@gmail.com>
2018-03-21 16:42:43 +0800

    Renders address to browser, but fetches using next.js server.

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import factory from '../ethereum/factory';
 
 class CampaignIndex extends Component {
-  async componentDidMount() {
+  
+  static async getInitialProps() { // static defines a class object and is a requirement of next.js.
     const campaigns = await factory.methods.getDeployedCampaigns().call();
 
-    console.log(campaigns);
+    return { campaigns };
   }
-  
+
   render() {
-    return <div>Campaigns Index!</div>
+    return <div>{this.props.campaigns[0]}</div>
   }
 }
 
