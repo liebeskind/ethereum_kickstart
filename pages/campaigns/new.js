c5b7d26b904c961dece76fb09dda6e755c919d9f
liebeskind <daniel.liebeskind@gmail.com>
2018-03-21 20:39:13 +0800

    Adds error checking

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
-import { Form, Button, Input } from 'semantic-ui-react';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import factory from '../../ethereum/factory'; // Lowercase because it's an instance of a contract.
 import web3 from '../../ethereum/web3';
 
 class CampaignNew extends Component {
   state = {
-    minimumContribution: ''
+    minimumContribution: '',
+    errorMessage: ''
   }
 
   onSubmit = async (event) => {
     event.preventDefault(); // Prevent browser from attempting to submit the form to our backend.
     
-    const accounts = await web3.eth.getAccounts();
-    await factory.methods
-    .createCampaign(this.state.minimumContribution)
-    .send({
-      from: accounts[0]
-    });
+    try {
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+      .createCampaign(this.state.minimumContribution)
+      .send({
+        from: accounts[0]
+      });
+    } catch (err) {
+      this.setState({ errorMessage: err.message });
+    }
   }
 
   render() {
@@ -25,7 +30,7 @@ class CampaignNew extends Component {
       <Layout>
         <h3>Create a Campaign</h3>
         
-        <Form onSubmit={this.onSubmit}>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input 
@@ -36,7 +41,7 @@ class CampaignNew extends Component {
                 this.setState({ minimumContribution: event.target.value })}
             />
           </Form.Field>
-
+          <Message error header="Oops!" content={this.state.errorMessage} />
           <Button primary>Create!</Button>
         </Form>
 
