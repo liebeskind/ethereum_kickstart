fa051a9d1949af13b9844f2003cfdee3201599c1
liebeskind <daniel.liebeskind@gmail.com>
2018-03-21 15:07:37 +0800

    Adds next.js, adds web3 instance and factory.

---
+++ ethereum/factory.js
@@ -0,0 +1,9 @@
+import web3 from './web3'; // Using instance from version we created.
+import campaignFactory from './build/campaignFactory.json';
+
+const instance = new web3.eth.Contract(
+  JSON.parse(campaignFactory.interface), // ABI
+  '0xAa69CbACbDD43C5281A47213661A506cb93822d6' // Where we deployed our campaignFactory contract previously.
+);
+
+export default instance;
