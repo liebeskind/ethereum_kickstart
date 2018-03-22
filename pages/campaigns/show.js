6344f274580bbd7d00a9ecb12b38ed183ea541ac
liebeskind <daniel.liebeskind@gmail.com>
2018-03-22 13:01:07 +0800

    Adds first property card (manager).

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,21 +1,44 @@
 import React, { Component } from 'react';
+import { Card } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
+
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
     const campaign = Campaign(props.query.address);
 
     const summary = await campaign.methods.getSummary().call();
-    console.log(summary);
 
-    return {};
+    return { // Pull the field order from campaign.sol contract.
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4]
+    };
+  }
+
+  renderCards() {
+    const { balance, manager, minimumContribution, requestsCount, approversCount } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: 'Address of Manager',
+        description: 'The Manager created this campaign and can create requests to withdraw money',
+        style: { overflowWrap: 'break-word' }
+      }
+    ];
+
+    return <Card.Group items={items} />
   }
 
   render() {
     return (
       <Layout>
         <h3>Campaign Details</h3>
+        {this.renderCards()}
       </Layout>
     );
   }
