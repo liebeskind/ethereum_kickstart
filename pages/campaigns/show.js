d358969101a4aa68309d7105bc6fcfbaf8eb88ab
liebeskind <daniel.liebeskind@gmail.com>
2018-03-22 13:23:36 +0800

    Adds grid structure for formatting.

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card } from 'semantic-ui-react';
+import { Card, Grid } from 'semantic-ui-react';
 import Layout from '../../components/Layout';
 import Campaign from '../../ethereum/campaign';
 import web3 from '../../ethereum/web3';
@@ -59,8 +59,14 @@ class CampaignShow extends Component {
     return (
       <Layout>
         <h3>Campaign Details</h3>
-        {this.renderCards()}
-        <ContributeForm />
+        <Grid>
+          <Grid.Column width={10}>
+            {this.renderCards()}
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <ContributeForm />
+          </Grid.Column>
+        </Grid>
       </Layout>
     );
   }
