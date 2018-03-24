c7d0923703769f3bec7f9f432f2f733ab3ba6c44
liebeskind <daniel.liebeskind@gmail.com>
2018-03-24 13:08:51 +0800

    Small formatting changes.

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -42,7 +42,7 @@ class RequestIndex extends Component {
         <h3>Requests</h3>
         <Link route={`/campaigns/${this.props.address}/requests/new`}>
           <a>
-            <Button primary>Add Request</Button>
+            <Button primary floated="right" style={{marginBottom: 10}}>Add Request</Button>
           </a>
         </Link>
         <Table>
@@ -61,6 +61,7 @@ class RequestIndex extends Component {
             {this.renderRows()}
           </Body>
         </Table>
+        <div>Found {this.props.requestCount} requests.</div>
       </Layout>
     );
   }
